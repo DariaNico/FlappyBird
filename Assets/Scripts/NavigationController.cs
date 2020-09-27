482d941cd4ad7ed05744b3a8ca623db3ed67bc43
Daria Tan <tdaria207@gmail.com>
2020-09-27 15:27:05 -0500

    Rename MainMenuController to NavigationController and enforce singleton on it

---
+++ Assets/Scripts/NavigationController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NavigationController : MonoBehaviour {
+    public static NavigationController instance;
+
+    void Awake() {
+        // Enforce the singleton pattern for NavigationController
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    public void PlayGame() {
+        SceneManager.LoadScene("FlappyGame");
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
