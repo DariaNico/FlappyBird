b0f9f156f572c082f02e57921be6404a7c927262
Daria Tan <tdaria207@gmail.com>
2020-09-27 02:23:38 -0500

    Add GameController to game

--- Assets/Scripts/Flapper.cs
+++ Assets/Scripts/Flapper.cs
@@ -2,59 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Flapper : MonoBehaviour 
-{
+public class Flapper : MonoBehaviour {
+    public static Flapper instance; //A reference to our game controller script so we can access it statically.
+
 	public float upForce;					//Upward force of the "flap".
 	private bool isDead = false;			//Has the player collided with a wall?
 
 	private Animator anim;					//Reference to the Animator component.
-	private Rigidbody2D rb2d;				//Holds a reference to the Rigidbody2D component of the bird.
+	private Rigidbody2D rb2d;				//Holds a reference to the Rigidbody2D component of the flapper.
 
+    void Awake() {
+        // Enforce the singleton pattern for GameController
+        //If we don't currently have a game controller...
+        if (instance == null)
+            //...set this one to be it...
+            instance = this;
+        //...otherwise...
+        else if (instance != this)
+            //...destroy this one because it is a duplicate.
+            Destroy(gameObject);
+    }
 
-	void Start()
-	{
+	void Start() {
 		//Get reference to the Animator component attached to this GameObject.
 		anim = GetComponent<Animator> ();
 		//Get and store a reference to the Rigidbody2D attached to this GameObject.
 		rb2d = GetComponent<Rigidbody2D>();
 	}
 
-	void Update()
-	{
-		//Don't allow control if the bird has died.
-		if (isDead == false) {
+	void Update() {
+		//Don't allow control if the flapper has died.
+		if (!isDead) {
 			//Look for input to trigger a "flap".
-			if (shouldFlap()) {
-				// Debug logs
-				print("I'm flapping!");
-				print(rb2d.velocity);
-				print(upForce);
+			if (ShouldFlap()) {
+				// DEBUG LOG
+				print("Flap Flap! velocity: " + rb2d.velocity + ", upForce: " + upForce);
 
 				//...tell the animator about it and then...
 				anim.SetTrigger("Flap");
-				//...zero out the birds current y velocity before...
+				//...zero out the flapper's current y velocity before...
 				rb2d.velocity = Vector2.zero;
 				//	new Vector2(rb2d.velocity.x, 0);
-				//..giving the bird some upward force.
+				//..giving the flapper some upward force.
 				rb2d.AddForce(new Vector2(0, upForce));
 			}
 		}
 	}
 
-	// Flapping controls here
-	private bool shouldFlap() {
-		return Input.GetButtonDown("Flap") || Input.GetMouseButtonDown(0);
-	}
-
-	void OnCollisionEnter2D(Collision2D other)
-	{
-		// Zero out the bird's velocity
+	void OnCollisionEnter2D(Collision2D other) {
+		// Zero out the flapper's velocity
 		rb2d.velocity = Vector2.zero;
-		// If the bird collides with something set it to dead...
+		// If the flapper collides with something set it to dead...
 		isDead = true;
 		//...tell the Animator about it...
-		anim.SetTrigger ("Die");
-		//...and tell the game control about it.
-		//GameControl.instance.BirdDied ();
+		anim.SetTrigger("Die");
+		//...and tell the game controller about it.
+		GameController.instance.FlapperDied();
 	}
+
+    // Flapping controls here
+    public bool ShouldFlap() {
+        return Input.GetButtonDown("Flap") || Input.GetMouseButtonDown(0);
+    }
 }
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+    public static GameController instance; //A reference to our game controller script so we can access it statically.
+    public Text scoreText; //A reference to the UI text component that displays the player's score.
+    public GameObject gameOverText; //A reference to the object that displays the text which appears when the player dies.
+
+    private int score = 0; //The player's score.
+    public bool gameOver = false; //Is the game over?
+    //public float scrollSpeed = -1.5f;
+
+    void Awake() {
+        // Enforce the singleton pattern for GameController
+        //If we don't currently have a game controller...
+        if (instance == null)
+            //...set this one to be it...
+            instance = this;
+        //...otherwise...
+        else if (instance != this)
+            //...destroy this one because it is a duplicate.
+            Destroy(gameObject);
+    }
+
+    void Update() {
+        //If the game is over and the player has pressed some input...
+        if (gameOver && Flapper.instance.ShouldFlap()) {
+            // DEBUG LOG
+            print("Restarting Game");
+
+            //...reload the current scene.
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    public void FlapperScored() {
+        //The flapper can't score if the game is over.
+        if (!gameOver) {
+            //If the game is not over, increase the score...
+            score++;
+            //...and adjust the score text.
+            scoreText.text = "Score: " + score.ToString();
+
+            // DEBUG LOG
+            print("Ting! Score: " + score);
+        }
+    }
+
+    public void FlapperDied() {
+        // DEBUG LOG
+        print("You Crashed! Game Over!");
+
+        //Activate the game over text.
+        gameOverText.SetActive(true);
+        //Set the game to be over.
+        gameOver = true;
+    }
+}
