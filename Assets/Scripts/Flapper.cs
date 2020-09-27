880414b297b349d385b66395cdfd5a58f7af8e97
Daria Tan <tdaria207@gmail.com>
2020-09-27 06:16:44 -0500

    Add MainMenu Scene and barebones test mode for scene objects

--- Assets/Scripts/Flapper.cs
+++ Assets/Scripts/Flapper.cs
@@ -3,29 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Flapper : MonoBehaviour {
-    public static Flapper instance; //A reference to our game controller script so we can access it statically.
+	public static Flapper instance; //A reference to our game controller script so we can access it statically.
 
-	public float upForce;					//Upward force of the "flap".
-	private bool isDead = false;			//Has the player collided with a wall?
+	public float upForce; //Upward force of the "flap".
+	private bool isDead = false; //Has the player collided with a wall?
 
-	private Animator anim;					//Reference to the Animator component.
-	private Rigidbody2D rb2d;				//Holds a reference to the Rigidbody2D component of the flapper.
+	private Animator anim; //Reference to the Animator component.
+	private Rigidbody2D rb2d; //Holds a reference to the Rigidbody2D component of the flapper.
 
-    void Awake() {
-        // Enforce the singleton pattern for GameController
-        //If we don't currently have a game controller...
-        if (instance == null)
-            //...set this one to be it...
-            instance = this;
-        //...otherwise...
-        else if (instance != this)
-            //...destroy this one because it is a duplicate.
-            Destroy(gameObject);
-    }
+	void Awake() {
+		// Enforce the singleton pattern for GameController
+		//If we don't currently have a game controller...
+		if (instance == null)
+			//...set this one to be it...
+			instance = this;
+		//...otherwise...
+		else if (instance != this)
+			//...destroy this one because it is a duplicate.
+			Destroy(gameObject);
+	}
 
 	void Start() {
 		//Get reference to the Animator component attached to this GameObject.
-		anim = GetComponent<Animator> ();
+		anim = GetComponent<Animator>();
 		//Get and store a reference to the Rigidbody2D attached to this GameObject.
 		rb2d = GetComponent<Rigidbody2D>();
 	}
@@ -49,19 +49,25 @@ public class Flapper : MonoBehaviour {
 		}
 	}
 
-	void OnCollisionEnter2D(Collision2D other) {
+	void OnCollisionEnter2D(Collision2D intruder) {
 		// Zero out the flapper's velocity
 		rb2d.velocity = Vector2.zero;
-		// If the flapper collides with something set it to dead...
-		isDead = true;
-		//...tell the Animator about it...
-		anim.SetTrigger("Die");
-		//...and tell the game controller about it.
-		GameController.instance.FlapperDied();
+
+		if (instance.tag == "Test") {
+			// DEBUG LOG
+			print("Test Collision");
+		} else {
+			// If the flapper collides with something set it to dead...
+			isDead = true;
+			//...tell the Animator about it...
+			anim.SetTrigger("Die");
+			//...and tell the game controller about it.
+			GameController.instance.FlapperDied();
+		}
 	}
 
-    // Flapping controls here
-    public bool ShouldFlap() {
-        return Input.GetButtonDown("Flap") || Input.GetMouseButtonDown(0);
-    }
+	// Flapping controls here
+	public bool ShouldFlap() {
+		return Input.GetButtonDown("Flap") || Input.GetMouseButtonDown(0);
+	}
 }
